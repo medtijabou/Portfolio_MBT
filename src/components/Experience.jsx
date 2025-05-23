87d9c0fa885177656bf522ea9d2f4e6f68fd64b4
medtijabou <tijarim7@gmail.com>
2025-05-23 22:29:48 +0200

    experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Calendar } from "lucide-react";
 
 const experiences = [
   { date: "09.2017 - 04.2024", title: "SSIAP 1 / SSIAP 2", company: "ONET MAIN SÉCURITÉ" },
@@ -18,8 +18,18 @@ const Experience = () => {
       <h2 className="experience-title">Expériences Professionnelles</h2>
       <div className="experience-list">
         {experiences.map((exp, index) => (
-          <div key={index} className="experience-card">
-            <p className="experience-date">{exp.date}</p>
+          <div
+            key={index}
+            className={`experience-card ${index % 2 === 0 ? "right" : "left"}`}
+          >
+            <p className="experience-date">
+  <span className="calendar-icon" dangerouslySetInnerHTML={{ __html: `
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="18" height="18" x="3" y="4" rx="2" ry="2"></rect><line x1="16" x2="16" y1="2" y2="6"></line><line x1="8" x2="8" y1="2" y2="6"></line><line x1="3" x2="21" y1="10" y2="10"></line><path d="M8 14h.01"></path><path d="M12 14h.01"></path><path d="M16 14h.01"></path><path d="M8 18h.01"></path><path d="M12 18h.01"></path><path d="M16 18h.01"></path></svg>
+  ` }} />
+  {exp.date}
+</p>
+
+
             <h3 className="experience-role">{exp.title}</h3>
             <p className="experience-company">{exp.company}</p>
           </div>
